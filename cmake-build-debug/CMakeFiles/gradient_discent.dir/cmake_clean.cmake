87286492e66157c7462613e65bcc70eba4999290
Alireza <alireza.lm69@gmail.com>
2020-08-19 00:50:18 -0300

    First version of the project.

---
+++ cmake-build-debug/CMakeFiles/gradient_discent.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gradient_discent.dir/main.cpp.o"
+  "gradient_discent"
+  "gradient_discent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gradient_discent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gradient_discent.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gradient_discent.
+# This may be replaced when dependencies are built.
