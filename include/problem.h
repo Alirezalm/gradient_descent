1593e0d7c04bca7e9c53e69ce15bc61990aed0c5
Alireza <alireza.lm69@gmail.com>
2020-08-19 00:56:15 -0300

    Project structure has be built.

---
+++ include/problem.h
@@ -0,0 +1,14 @@
+//
+// Created by alireza on 8/19/20.
+//
+
+#ifndef GRADIENT_DISCENT_PROBLEM_H
+#define GRADIENT_DISCENT_PROBLEM_H
+
+
+class problem {
+
+};
+
+
+#endif //GRADIENT_DISCENT_PROBLEM_H
---
+++ src/problem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alireza on 8/19/20.
+//
+
+#include "../include/problem.h"
